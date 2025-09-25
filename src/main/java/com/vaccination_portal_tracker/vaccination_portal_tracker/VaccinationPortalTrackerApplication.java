e9b6e228ef62c7ece9ebeb017f82a129786d817e
IT24101932 <it24101932@my.sliit.lk>
2025-09-25 16:04:24 +0530

    create file structure

---
+++ src/main/java/com/vaccination_portal_tracker/vaccination_portal_tracker/VaccinationPortalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vaccination_portal_tracker.vaccination_portal_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationPortalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationPortalTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vaccination_portal_tracker/vaccination_portal_tracker/VaccinationPortalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccination_portal_tracker.vaccination_portal_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationPortalTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
